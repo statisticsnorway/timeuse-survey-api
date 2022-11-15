08c97ed9c2efc39e7ee9d966b0fcd1994d29f1ea
SanderOpperud <sanderopperud@hotmail.com>
2022-11-15 14:20:25 +0100

    Legger til Spring Boot og filstruktur.

---
+++ src/test/kotlin/no/ssb/timeusesurveyapi/TimeuseSurveyApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package no.ssb.timeusesurveyapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TimeuseSurveyApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
