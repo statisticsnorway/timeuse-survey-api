d5c34335861ee3bdece211b4252f0aea95db2d77
Sander Opperud Odden <60642441+SanderOpperud@users.noreply.github.com>
2022-11-15 15:00:01 +0100

    Legger til Spring Boot

---
+++ src/main/kotlin/no/ssb/timeusesurveyapi/TimeuseSurveyApiApplication.kt
@@ -0,0 +1,11 @@
+package no.ssb.timeusesurveyapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TimeuseSurveyApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<TimeuseSurveyApiApplication>(*args)
+}
