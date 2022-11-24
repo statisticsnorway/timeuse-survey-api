c78a3b1fa7f3a40b3389c56c3d3a240547f6f78a
SanderOpperud <sanderopperud@hotmail.com>
2022-11-24 13:21:21 +0100

    Fjerner ubrukt lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,6 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	//implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
