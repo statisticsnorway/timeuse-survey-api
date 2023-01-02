e6c247620c28acb26ac48e7d83edeac8dd4f1ec5
SanderOpperud <sanderopperud@hotmail.com>
2023-01-02 10:01:04 +0100

    Oppdaterer pakker * Closes #32 * Closes #31 * Closes #30

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.0.0"
+	id("org.springframework.boot") version "3.0.1"
 	id("io.spring.dependency-management") version "1.1.0"
-	kotlin("jvm") version "1.7.22"
-	kotlin("plugin.spring") version "1.7.22"
+	kotlin("jvm") version "1.8.0"
+	kotlin("plugin.spring") version "1.8.0"
 }
 
 group = "no.ssb"
